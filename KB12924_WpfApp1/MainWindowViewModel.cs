bbfd4fb7b0d1113c6af4cb4bd6052c5e5823f2bb
norikois <33947244+norikois@users.noreply.github.com>
2024-02-14 12:51:52 +0900

    初回コミット

---
+++ KB12924_WpfApp1/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using KB12924_WpfApp1.Infrastructure;
+using KB12924_WpfApp1.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KB12924_WpfApp1;
+internal class MainWindowViewModel : ObservableObject
+{
+    private ObservableCollection<Visitors> _visitorDistribution;
+
+    public ObservableCollection<Visitors> VisitorDistribution
+    {
+        get { return _visitorDistribution; }
+        set { _visitorDistribution = value; OnPropertyChanged(); }
+    }
+
+    public MainWindowViewModel()
+    {
+        _visitorDistribution = new()
+        {
+            new() { AgeClass = "0-9", January = 20, February = 24, March = 18 },
+            new() { AgeClass = "10-19", January = 10, February = 11, March = 20 },
+            new() { AgeClass = "20-29", January = 11, February = 9, March = 14 },
+            new() { AgeClass = "30-39", January = 23, February = 30, March = 20 },
+        };
+    }
+}
---
+++ KB12924_WpfApp1/Model/Visitors.cs
@@ -0,0 +1,46 @@
+﻿using KB12924_WpfApp1.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KB12924_WpfApp1.Model;
+internal class Visitors : ObservableObject
+{
+    private String _ageClass = "";
+
+    public String AgeClass
+    {
+        get { return _ageClass; }
+        set { _ageClass = value; OnPropertyChanged(); }
+    }
+
+    private int _january;
+
+    public int January
+    {
+        get { return _january; }
+        set { _january = value; OnPropertyChanged(); }
+    }
+
+    private int _february;
+
+    public int February
+    {
+        get { return _february; }
+        set { _february = value; OnPropertyChanged(); }
+    }
+
+    private int _march;
+
+    public int March
+    {
+        get { return _march; }
+        set { _march = value; OnPropertyChanged(); }
+    }
+
+    public Visitors()
+    {
+    }
+}
